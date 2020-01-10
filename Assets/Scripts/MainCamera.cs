42d0556919ca9f57585accaa2abab79718fb4750
Quinn Larson <quinn@ramblehousegames.com>
2020-01-10 20:50:35 +0900

    Initial Commit

---
+++ Assets/Scripts/MainCamera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    public static MainCamera Inst;
+
+    private void Awake()
+    {
+        if (MainCamera.Inst == null)
+            Inst = this;
+        else
+            Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/SceneFlowManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneFlowManager : MonoBehaviour
+{
+    public static SceneFlowManager Inst;
+
+    public SceneState currentState = new StartState();
+
+    private void Awake()
+    {
+        if (SceneFlowManager.Inst == null)
+            Inst = this;
+    }
+
+    private void Start()
+    {
+        currentState.Start();
+    }
+
+    private void Update()
+    {
+        
+    }
+}
+
+public class SceneState
+{
+    public virtual void Start()
+    {
+        Debug.Log("SceneState.Start not implemented");
+    }
+}
+
+public class StartState : SceneState
+{
+    public override void Start()
+    {
+        SceneManager.LoadSceneAsync("GameScene", LoadSceneMode.Additive);
+    }
+}
